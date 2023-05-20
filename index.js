55e20b75703567fe96450901ab80db9429a4291d
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-20 21:09:06 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    let message = []
+    for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message
+}
+    
+writeCards(names, "surprise");
+
+function countDown(number) { 
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+countDown(10)
+
